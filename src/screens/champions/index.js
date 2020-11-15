115f5349e3be684cca2b7ad66c361628bf0b26f6
Luiz Mauro <luizmetec@hotmail.com>
2020-11-15 00:24:47 -0300

    montando screen champions e styles

--- src/screens/champions/index.js
+++ src/screens/champions/index.js
@@ -1,13 +1,63 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, SafeAreaView, ActivityIndicator } from "react-native";
 
-import { Container } from "./styles";
+import api from "../../services/api";
+
+import { useNavigation } from "@react-navigation/native";
+
+import {
+  Container,
+  ContainerLoading,
+  ButtonChampion,
+  NameChampion,
+  ImageChampion,
+} from "./styles";
 
 const Champions = () => {
+  const { navigate } = useNavigation();
+  const [loading, setLoading] = useState(true);
+  const [champions, setChampions] = useState([]);
+
+  async function loadChampions() {
+    const response = await api.get("champion.json");
+    const championsObjJson = Object.values(response.data.data);
+
+    setChampions(championsObjJson);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadChampions();
+  }, []);
+
+  if (loading) {
+    return (
+      <ContainerLoading>
+        <ActivityIndicator size="large" color="#c38f2c" />
+      </ContainerLoading>
+    );
+  }
+
   return (
-    <Container>
-      <Text>Home</Text>
-    </Container>
+    <ScrollView>
+      <SafeAreaView>
+        <Container>
+          {champions.map((champ) => (
+            <ButtonChampion
+              key={champ.key}
+              onPress={() => navigate("Champion-Details", { champ })}
+            >
+              <ImageChampion
+                source={{
+                  uri: `http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${champ.image.full}`,
+                }}
+              />
+              <NameChampion>{champ.name}</NameChampion>
+            </ButtonChampion>
+          ))}
+        </Container>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
--- src/screens/champions/styles.js
+++ src/screens/champions/styles.js
@@ -1,7 +1,38 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
+  display: flex;
   flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+  background-color: #000;
+  padding-bottom: 50px;
   justify-content: center;
   align-items: center;
+`;
+
+export const ContainerLoading = styled.View`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  background-color: #000;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonChampion = styled.TouchableOpacity`
+  width: 80px;
+  height: 80px;
+  margin: 9px;
+  border-radius: 6px;
+`;
+
+export const NameChampion = styled.Text`
+  color: #fff;
+  text-align: center;
+`;
+
+export const ImageChampion = styled.Image`
+  height: 100%;
+  width: 100%;
 `;
