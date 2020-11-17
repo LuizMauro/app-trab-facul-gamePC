70a03b5b746e7b3624680e6639096a5d0fa8e274
Luiz Mauro <luizmetec@hotmail.com>
2020-11-17 09:15:27 -0300

    add header token

--- src/services/apiLoL.js
+++ src/services/apiLoL.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const apiLoL = axios.create({
   baseURL: "https://br1.api.riotgames.com/lol/",
+  headers: { "X-Riot-Token": "RGAPI-9b5d6c8e-1f11-4b62-b6e4-d0db0e0febc6" },
 });
 
 export default apiLoL;
