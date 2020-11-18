d461e73b318e5c4847c69c092c8bfef21ab1ec20
Nilodam <nickricardo@hotmail.com>
2020-11-18 19:30:19 -0300

    Modify ItemDetails

--- src/services/apiLoL.js
+++ src/services/apiLoL.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiLoL = axios.create({
   baseURL: "https://br1.api.riotgames.com/lol/",
-  headers: { "X-Riot-Token": "RGAPI-788049a3-e750-48d3-aa9a-1ed3fbb47e1e" },
+  headers: { "X-Riot-Token": "RGAPI-1f9e9b1b-50e6-482c-93c0-86c948a4aeeb" },
 });
 
 export default apiLoL;
