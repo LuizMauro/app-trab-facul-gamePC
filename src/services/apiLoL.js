df9d445cd3b8018446b7a56a3201c366d3a74b5c
Luiz Mauro <luizmetec@hotmail.com>
2020-11-18 20:47:10 -0300

    mudando token

--- src/services/apiLoL.js
+++ src/services/apiLoL.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiLoL = axios.create({
   baseURL: "https://br1.api.riotgames.com/lol/",
-  headers: { "X-Riot-Token": "RGAPI-788049a3-e750-48d3-aa9a-1ed3fbb47e1e" },
+  headers: { "X-Riot-Token": "RGAPI-98ef3433-6c4d-438c-b641-2e8b003d59a8" },
 });
 
 export default apiLoL;
